128ed312dd52d13df166fbab4b7e56c820940c32
Sahil-husen <sahilhussainsahil76@gmail.com>
2024-05-31 16:41:53 +0530

    Progress Bar Added

--- script.js
+++ script.js
@@ -35,3 +35,12 @@ document.addEventListener("DOMContentLoaded", function () {
       });
     });
   });
+
+
+  // Progress Bar
+  const filled = document.querySelector('.filled');
+  function update(){
+    filled.style.width =  `${((window.scrollY) /(document.body.scrollHeight - window.innerHeight)*100 )}%`
+    requestAnimationFrame(update);
+  }
+  update();
